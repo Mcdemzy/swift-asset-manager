6a88cc445db3cc68fb86bf48e1609358cc4da325
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-18 23:50:27 +0100

    Created why use SAM section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Benefit from "./components/benefit/Benefit";
 
 const App = () => {
   return (
