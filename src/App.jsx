99c9a8005ae6615ce8d6d464edd5fee0d9ddd0fc
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-19 10:20:46 +0100

    Why use completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import WhyUse from "./components/why use section/WhyUse";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <WhyUse />
     </>
   );
 };
---
+++ src/components/why use section/WhyUse.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const WhyUse = () => {
+  return (
+    <>
+      <section className="w-full h-[100vh] bg-white  pt-[48px] px-[112px]">
+        <h2 className="text-[#7C5CFC] font-[37.5rem] text-[1.25rem]">
+          Why Use Swift Asset Manager
+        </h2>
+        <div className="flex gap-[150px] mt-5">
+          <p className="text-[#040815] max-w-[15.625rem] text-[2.5rem] font-[700] leading-[3.75rem] tracking-[-0.075rem]">
+            Easy, Simple, Affordable
+          </p>
+          <p className="max-w-[420px] text-[#596780] font-[400px] text-[20px] leading-[30px] tracking-[-0.4px]">
+            Our platform helps your business in managing assets and expenses.
+            These are some of the reasons why you should use our platform in
+            managing assets.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default WhyUse;
