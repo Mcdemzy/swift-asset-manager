e40926e9b972f3ea1deb4696af471d06e2313233
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-28 12:35:46 +0100

    Deleted why use component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import WhyUse from "./components/why use section/WhyUse";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <WhyUse />
     </>
   );
 };
