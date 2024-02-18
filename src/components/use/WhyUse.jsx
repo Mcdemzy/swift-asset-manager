c911d5be7849375925cff0aff15c0b0c7f49f43f
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-18 10:40:10 +0100

    Created second Why Use Component

--- src/components/use/WhyUse.jsx
+++ src/components/use/WhyUse.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import WhyUseImage1 from "../../assets/whyUseImage1.jpg";
+import WhyUseImage2 from "../../assets/whyUseImage2.png";
+import Image from "../../assets/image.jpeg";
 
 const WhyUse = () => {
-  return <div>WhyUse</div>;
+  return (
+    <>
+      <section className="pt-[80px] w-[90%] m-auto mt-[10px]">
+        <section>
+          <span className="text-[1.25rem] font-[600] leading-[30px] text-[#7C5CFC] tracking-[-2%]">
+            WHY USE SAM
+          </span>
+          <div className="flex gap-[100px] mt-[40px]">
+            <h2 className="text-[2.5rem] font-[700] leading-[40px] max-w-[245px] text-[#040815] tracking-[-3%]">
+              Easy, Simple, Affordable
+            </h2>
+            <p className="max-w-[496px] text-[1.25rem] font-[400] leading-[30px] tracking-[-2%] text-[#596780]">
+              Our platform helps your business in managing expenses. These are
+              some of the reasons why you should use our platform in managing
+              business finances.
+            </p>
+          </div>
+        </section>
+
+        {/* WHY USE BOXES */}
+
+        <section className=" mt-[100px] m-auto justify-center flex ga">
+          <div>
+            <div className="bg-[#F3F5F7] w-[384px] h-[384px] rounded-xl pt-[50px]">
+              <img
+                src={WhyUseImage1}
+                alt=""
+                className="w-[90%] rounded-xl h-full"
+              />
+            </div>
+
+            <div className="mt-[25px]">
+              <h3 className=" text-[1.5rem] font-[600] leading-[36px] tracking-[-3%] text-[#040815]">
+                Automatic Invoice Payment
+              </h3>
+              <p className="max-w-[384px] text-[#596780] text-[1rem] font-[400] leading-[24px] tracking-[-2%] mt-2">
+                No need to pay manually, we provide automatic invoice payment
+                service! Set a payment schedule and you're done, it's that easy!
+              </p>
+            </div>
+          </div>
+
+          {/*  */}
+
+          <div>
+            <div className="bg-[#F3F5F7] w-[384px] h-[384px] rounded-xl pt-[50px]">
+              <img
+                src={WhyUseImage1}
+                alt=""
+                className="w-[90%] rounded-xl h-full"
+              />
+            </div>
+
+            <div className="mt-[25px]">
+              <h3 className=" text-[1.5rem] font-[600] leading-[36px] tracking-[-3%] text-[#040815]">
+                Automatic Invoice Payment
+              </h3>
+              <p className="max-w-[384px] text-[#596780] text-[1rem] font-[400] leading-[24px] tracking-[-2%] mt-2">
+                No need to pay manually, we provide automatic invoice payment
+                service! Set a payment schedule and you're done, it's that easy!
+              </p>
+            </div>
+          </div>
+
+          {/*  */}
+
+          <div>
+            <div className="bg-[#F3F5F7] w-[384px] h-[384px] rounded-xl pt-[50px]">
+              <img
+                src={WhyUseImage1}
+                alt=""
+                className="w-[90%] rounded-xl h-full"
+              />
+            </div>
+
+            <div className="mt-[25px]">
+              <h3 className=" text-[1.5rem] font-[600] leading-[36px] tracking-[-3%] text-[#040815]">
+                Automatic Invoice Payment
+              </h3>
+              <p className="max-w-[384px] text-[#596780] text-[1rem] font-[400] leading-[24px] tracking-[-2%] mt-2">
+                No need to pay manually, we provide automatic invoice payment
+                service! Set a payment schedule and you're done, it's that easy!
+              </p>
+            </div>
+          </div>
+        </section>
+      </section>
+
+      {/* WHY USE SECTION 2 */}
+    </>
+  );
 };
 
 export default WhyUse;
