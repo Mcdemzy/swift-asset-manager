d880dce2658f46cdd2c77dbd1969089d6c931c45
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-19 23:39:02 +0100

    Created Works Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import WhyUse from "./components/use/WhyUse";
+import Success from "./components/success/Success";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <WhyUse />
+      <Success />
     </>
   );
 };
---
+++ src/components/success/Success.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <>
+      <section className="h-[100vh] bg-black">Success</section>
+    </>
+  );
+};
+
+export default Success;
