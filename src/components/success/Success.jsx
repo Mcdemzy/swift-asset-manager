28e1c004b40f20f2316c76c7c70dcc44a85c4984
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-21 20:49:01 +0100

    Added Image

--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WhyUseImage1 from "../../assets/whyUseImage1.jpg";
 import { FaCircleCheck } from "react-icons/fa6";
+import { MdCancel } from "react-icons/md";
 
 const Success = () => {
   return (
     <>
+      {/* FIRST SUCCESS STORY SECTION  */}
       <section className="bg-white text-black w-[90%] m-auto pt-[100px] pb-[100px]">
         <h3 className="text-[#7C5CFC] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%]">
           INCREASE PRODUCTIVITY
@@ -47,6 +49,63 @@ const Success = () => {
                 <p className="text-[#0D121F] text-[1.13rem] font-[500] leading-[27px] tracking-[-2%]">
                   Arrange your business expenses by date, name, etc., with just
                   one click.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
+          <div className="max-w-[486px] max-h-[434px] rounded-xl">
+            <img src={WhyUseImage1} alt="" />
+          </div>
+        </section>
+      </section>
+
+      {/* SECOND SUCCESS STORY SECTION */}
+
+      <section className="bg-white text-black w-[90%] m-auto pt-[100px] pb-[100px]">
+        <h3 className="text-[#7C5CFC] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%]">
+          INCREASE PRODUCTIVITY
+        </h3>
+        <p className="text-[#0D121F] max-w-[634px] text-[2.5rem] font-[700] leading-[60px] tracking-[-3%]">
+          Reduce Time in Doing Manual Work Managing Expenses
+        </p>
+
+        <section className="flex justify-between">
+          <div>
+            <div className="w-[368px] h-[56px] bg-[#F3F5F7] rounded-[30px] flex mt-[50px]">
+              <div className=" w-[195px] rounded-[30px] flex justify-center items-center text-[#90A3BF]">
+                With SAM
+              </div>
+              <div className="bg-[#7C5CFC] w-[169px] ml-[20px] rounded-[30px] flex justify-center items-center text-[#FFFFFF]">
+                Without SAM
+              </div>
+            </div>
+            {/*  */}
+            <div className="max-w-[526px] h-[294px] mt-[60px] flex flex-col gap-7">
+              <h4 className="text-[#0D121F] text-[1.5rem] font-[700] leading-[36px] tracking-[-3%]">
+                Taking too long to tidy up your assets makes your business
+                unproductive
+              </h4>
+              <div className="max-w-[490px] flex">
+                <FaCircleCheck className="text-[#FF4423] mt-[6px] mr-[5px] text-[1.2rem]" />
+                <p className="text-[#0D121F] text-[1.13rem] font-[500] leading-[27px] tracking-[-2%]">
+                  Complex recording process due to every administrative file in
+                  a different place.
+                </p>
+              </div>
+              <div className="max-w-[490px] flex">
+                <FaCircleCheck className="text-[#FF4423] mt-[6px] mr-[5px] text-[1.5rem]" />
+                <p className="text-[#0D121F] text-[1.13rem] font-[500] leading-[27px] tracking-[-2%]">
+                  Need more effort to pay manually one by one invoice because
+                  there is no payment accommodation.
+                </p>
+              </div>
+              <div className="max-w-[490px] flex">
+                <FaCircleCheck className="text-[#FF4423] mt-[6px] mr-[5px] text-[1.5rem]" />
+                <p className="text-[#0D121F] text-[1.13rem] font-[500] leading-[27px] tracking-[-2%]">
+                  Manual data arranging needs a long time because the different
+                  months/years are not in the same place.
                 </p>
               </div>
             </div>
