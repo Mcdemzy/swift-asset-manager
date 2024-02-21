e27a30a183852a676da5cdb5ca90faed97f6dacc
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-21 18:47:13 +0100

    Completed Works Component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -5,7 +5,7 @@ const Works = () => {
   return (
     <>
       <section className="bg-[#0D121F]">
-        <section className="w-[85%] m-auto ">
+        <section className="w-[85%] m-auto flex gap-[60px] pt-[200px] pb-[100px]">
           <div className="max-w-[588px] bg-[#F3F5F7] max-h-[536px] rounded-xl">
             <img
               src={WhyUseImage1}
@@ -21,12 +21,31 @@ const Works = () => {
             <h4 className="text-[#FFFFFF] text-[2.5rem] font-[700] leading-[60px] tracking-[-3%]">
               Few Easy Steps and Done
             </h4>
-            <p className="text-[#90A3BF] text-[1.25rem] font-[400] leading-[]">
+            <p className="text-[#90A3BF] text-[1.25rem] font-[400] leading-[30px] tracking-[-2%]">
               In just few easy step, you are all set to manage your business
-              finances. Manage all expenses with Spend.In all in one place.
+              finances. Manage all assets with SAM all in one place.
             </p>
 
-            <div></div>
+            <div className="bg-[#1A202C] max-h-[300px] p-[20px] flex flex-col gap-[30px]  rounded-xl">
+              <h5 className="flex ">
+                <span className="bg-[#7C5CFC] w-[40px] h-[40px] rounded-full flex items-center justify-center text-[1.25rem]">
+                  1
+                </span>
+                <span className="pt-2 ml-2"> Register your SAM account.</span>
+              </h5>
+              <h5 className="flex ">
+                <span className="bg-[#7C5CFC] w-[40px] h-[40px] rounded-full flex items-center justify-center text-[1.25rem]">
+                  1
+                </span>
+                <span className="pt-2 ml-2"> Register your SAM account.</span>
+              </h5>
+              <h5 className="flex ">
+                <span className="bg-[#7C5CFC] w-[40px] h-[40px] rounded-full flex items-center justify-center text-[1.25rem]">
+                  1
+                </span>
+                <span className="pt-2 ml-2"> Register your SAM account.</span>
+              </h5>
+            </div>
           </div>
         </section>
       </section>
