2771d71384faaf485b14be3addc78a498d681bf7
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-23 23:36:21 +0100

    Completed Testimonial Component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TestimonialImage1 from "../../assets/testimonialImage1.png";
 import TestimonialImage2 from "../../assets/testimonialImage2.png";
+import TestimonialImage3 from "../../assets/testimonialImage3.png";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
 
@@ -50,12 +51,11 @@ const Testimonials = () => {
           </div>
           <div className="bg-[#1A202C] w-[384px] h-[363px] rounded-xl p-[20px] ">
             <h4 className="text-[#FFFFFF] text-[1.125rem] font-[600] leading-[27px] tracking-[-2]">
-              It’s just incredible!
+              Satisfied User Here!
             </h4>
             <p className="text-[#F3F5F7] text-[1rem] font-[500] leading-[24px] tracking-[-2%] mt-5 border-b-2 pb-10 border-[#90A3BF]">
-              It’s just 1 month since I’m using SAM to manage my business
-              assets, but the result is very satisfying! My business finance is
-              now more neat than before, thanks to SAM!
+              Never thought that with Spend.In managing my business expenses is
+              so easy! Been using this platform for 3 months and still counting!
             </p>
             {/* User Section */}
 
@@ -77,33 +77,36 @@ const Testimonials = () => {
           </div>
           <div className="bg-[#1A202C] w-[384px] h-[363px] rounded-xl p-[20px] ">
             <h4 className="text-[#FFFFFF] text-[1.125rem] font-[600] leading-[27px] tracking-[-2]">
-              It’s just incredible!
+              No doubt, SAM is the best!
             </h4>
             <p className="text-[#F3F5F7] text-[1rem] font-[500] leading-[24px] tracking-[-2%] mt-5 border-b-2 pb-10 border-[#90A3BF]">
-              It’s just 1 month since I’m using SAM to manage my business
-              assets, but the result is very satisfying! My business finance is
-              now more neat than before, thanks to SAM!
+              “The best”! That’s what I want to say to this platform, didn’t
+              know that there’s a platform to help you manage your business
+              expenses like this! Very recommended to you who have a big
+              business!
             </p>
             {/* User Section */}
 
             <div className="mt-[40px] flex gap-6">
               <img
-                src={TestimonialImage1}
+                src={TestimonialImage3}
                 alt="Testimonial Image"
                 className="rounded-[12px] w-[70px] h-[70px]"
               />
               <div className="mt-5">
                 <h5 className="text-[#FFFFFF] text-[1.125rem] font-[700] leading-[27px] tracking-[-3%]">
-                  Jimmy Bartney
+                  Moritika Kazuki
                 </h5>
                 <p className="text-[#C3D4E9] text-[0.875rem] font-[400] leading-[21px] tracking-[-2%]">
-                  Product Manager at Picko Lab
+                  Finance Manager at Mangan
                 </p>
               </div>
             </div>
           </div>
         </section>
+
         {/* ARROWS */}
+
         <div className="flex m-auto items-center justify-center mt-16 gap-8">
           <a href="#!">
             <FaArrowLeftLong className="bg-[#1A202C] text-[#C3D4E9] p-3 text-[3rem] rounded-full" />
