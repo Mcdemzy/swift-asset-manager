b258f4384f6914ab644d352b0abf34f04a7dfa8f
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-26 13:26:31 +0100

    Editing Pricing Component list

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -90,20 +90,21 @@ const Pricing = () => {
             <div className="flex items-center gap-2">
               <RiHeartsFill className="text-[#7C5CFC] text-[2rem]" />
               <h4 className="text-[#0D121F] text-[2rem] font-[600] leading-[48px] tracking-[-3%]">
-                Free
+                Pro
               </h4>
             </div>
             <p className="text-[#596780] text-[1rem] font-[400] leading-[24px] tracking-[-2%]">
-              Perfect plan to get started
+              Perfect plan for professionals!
             </p>
             <p className="text-[#0D121F] text-[2.5rem] font-[600] leading-[60px] tracking-[-3%] flex items-center gap-2 mt-6 mb-6">
-              $0
+              $12
               <span className="text-[1rem] text-[#596780] font-[500] leading-[27px] tracking-[-2%]">
                 /month
               </span>
             </p>
             <p className="text-[#1A202C] max-w-[320px] text-[1rem] font-[400] leading-[24px] tracking-[-2%] mb-8">
-              A free plan grants you access to some cool features of SAM.
+              For professionals only! Start arranging your expenses with our
+              best templates.
             </p>
 
             {/* CONTAINER DETAILS */}
