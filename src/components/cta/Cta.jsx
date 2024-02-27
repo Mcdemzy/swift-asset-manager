fa837d0b82efacb0f860797251546290cdc175d2
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-27 13:52:18 +0100

    Added Image

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <>
+      <section className="bg-[#0D121F] h-[100vh]">
+        <div className="p-[40px]">
+          <h3 className="text-[#7C5CFC] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%]">
+            DOWNLOAD NOW!
+          </h3>
+          <h4 className="text-[#FFFFFF] max-w-[454px] text-[2.5rem] font-[700] leading-[60px] tracking-[-3%]">
+            Start Tracking Your Business Assets Today
+          </h4>
+          <p className="text-[#596780] max-w-[568px] text-[1.25rem] font-[400] leading-[30px] tracking-[-2%] mt-4 mb-8">
+            Are you ready to make your business more organized? <br /> Sign up
+            to SAM now!
+          </p>
+          <button className="w-[187px] h-[52px] bg-[#7C5CFC] flex justify-center items-center rounded-[30px] mt-4 text-[1rem] text-[#FFFFFF] font-[600] leading-[24px] tracking-[-2%]">
+            Get a Free demo
+          </button>
+        </div>
+
+        {/*  */}
+
+        <div>
+          <img src="" alt="" />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Cta;
