16200584574e6dc9a95da867d80fdde01b526b99
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-02-27 15:16:57 +0100

    Editing Footer Component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,104 +4,106 @@ const Footer = () => {
   return (
     <>
       <section className="footer__container bg-[#ff4678] h-[100vh]">
-        {/*  */}
-        <div>
-          <img src="" alt="" />
-          <h3></h3>
-          <p>Data visualization, and expense management for your business.</p>
-        </div>
+        <section>
+          {/*  */}
+          <div>
+            <img src="" alt="" />
+            <h3></h3>
+            <p>Data visualization, and expense management for your business.</p>
+          </div>
 
-        {/*  */}
+          {/*  */}
 
-        <div>
-          <p>Product</p>
-          <ul>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-            <li>
-              <a href="#!">Insights</a>
-            </li>
-            <li>
-              <a href="#!">Reimbursements</a>
-            </li>
-            <li>
-              <a href="#!">Virtual Assistant</a>
-            </li>
-            <li>
-              <a href="#!">Artificial Intelligence</a>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <p>Product</p>
+            <ul>
+              <li>
+                <a href="#!">Digital Invoice</a>
+              </li>
+              <li>
+                <a href="#!">Insights</a>
+              </li>
+              <li>
+                <a href="#!">Reimbursements</a>
+              </li>
+              <li>
+                <a href="#!">Virtual Assistant</a>
+              </li>
+              <li>
+                <a href="#!">Artificial Intelligence</a>
+              </li>
+            </ul>
+          </div>
 
-        {/*  */}
+          {/*  */}
 
-        <div>
-          <p>Company</p>
-          <ul>
-            <li>
-              <a href="#!">About Us</a>
-            </li>
-            <li>
-              <a href="#!">Newsletters</a>
-            </li>
-            <li>
-              <a href="#!">Our Partners</a>
-            </li>
-            <li>
-              <a href="#!">Career</a>
-            </li>
-            <li>
-              <a href="#!">Contact Us</a>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <p>Company</p>
+            <ul>
+              <li>
+                <a href="#!">About Us</a>
+              </li>
+              <li>
+                <a href="#!">Newsletters</a>
+              </li>
+              <li>
+                <a href="#!">Our Partners</a>
+              </li>
+              <li>
+                <a href="#!">Career</a>
+              </li>
+              <li>
+                <a href="#!">Contact Us</a>
+              </li>
+            </ul>
+          </div>
 
-        {/*  */}
+          {/*  */}
 
-        <div>
-          <p>Resources</p>
-          <ul>
-            <li>
-              <a href="#!">Blog</a>
-            </li>
-            <li>
-              <a href="#!">Pricing</a>
-            </li>
-            <li>
-              <a href="#!">FAQ</a>
-            </li>
-            <li>
-              <a href="#!">Events</a>
-            </li>
-            <li>
-              <a href="#!">Ebook & Guide</a>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <p>Resources</p>
+            <ul>
+              <li>
+                <a href="#!">Blog</a>
+              </li>
+              <li>
+                <a href="#!">Pricing</a>
+              </li>
+              <li>
+                <a href="#!">FAQ</a>
+              </li>
+              <li>
+                <a href="#!">Events</a>
+              </li>
+              <li>
+                <a href="#!">Ebook & Guide</a>
+              </li>
+            </ul>
+          </div>
 
-        {/*  */}
+          {/*  */}
 
-        <div>
-          <p>Product</p>
-          <ul>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-            <li>
-              <a href="#!">Digital Invoice</a>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <p>Follow Us</p>
+            <ul>
+              <li>
+                <a href="#!">LinkedIn</a>
+              </li>
+              <li>
+                <a href="#!">Twitter</a>
+              </li>
+              <li>
+                <a href="#!">Instagram</a>
+              </li>
+              <li>
+                <a href="#!">Facebook</a>
+              </li>
+              <li>
+                <a href="#!">YouTube</a>
+              </li>
+            </ul>
+          </div>
+        </section>
       </section>
     </>
   );
