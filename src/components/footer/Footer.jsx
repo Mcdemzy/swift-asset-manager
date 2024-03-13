2221b02096792dde6f1b586a27978ae1d10e4ee6
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-03-13 12:54:02 +0100

    Re-editing Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <section className="footer__container bg-[#ff4678] h-[100vh]">
-        <section>
+      <section className="bg-[#ffffff] text-black  pt-[130px] pb-[200px]">
+        <section className="footer__container grid">
           {/*  */}
-          <div>
+          <div className="">
             <img src="" alt="" />
             <h3></h3>
             <p>Data visualization, and expense management for your business.</p>
@@ -15,101 +15,110 @@ const Footer = () => {
           {/*  */}
 
           <div>
-            <p>Product</p>
-            <ul>
-              <li>
+            <p className="text-[#1A202C] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%] mb-4">
+              Product
+            </p>
+            <ul className="leading-[40px]">
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Digital Invoice</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Insights</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Reimbursements</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Virtual Assistant</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Artificial Intelligence</a>
               </li>
             </ul>
           </div>
 
-          {/*  */}
+          {/* Company  */}
 
           <div>
-            <p>Company</p>
-            <ul>
-              <li>
+            <p className="text-[#1A202C] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%] mb-4">
+              Company
+            </p>
+            <ul className="leading-[40px]">
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">About Us</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Newsletters</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Our Partners</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Career</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Contact Us</a>
               </li>
             </ul>
           </div>
 
-          {/*  */}
+          {/* Resources  */}
 
           <div>
-            <p>Resources</p>
-            <ul>
-              <li>
+            <p className="text-[#1A202C] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%] mb-4">
+              Resources
+            </p>
+            <ul className="leading-[40px]">
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Blog</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Pricing</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">FAQ</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Events</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Ebook & Guide</a>
               </li>
             </ul>
           </div>
 
-          {/*  */}
+          {/* Follow Us */}
 
           <div>
-            <p>Follow Us</p>
-            <ul>
-              <li>
+            <p className="text-[#1A202C] text-[1.25rem] font-[600] leading-[30px] tracking-[-2%] mb-4">
+              Follow Us
+            </p>
+            <ul className="leading-[40px]">
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">LinkedIn</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Twitter</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Instagram</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">Facebook</a>
               </li>
-              <li>
+              <li className="text-[#596780] text-[1rem] font-[400]">
                 <a href="#!">YouTube</a>
               </li>
             </ul>
           </div>
         </section>
+
+        {/*  */}
+
+        <div></div>
       </section>
     </>
   );
 };
 
 export default Footer;
-
-//   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-// Editing Footer Component
